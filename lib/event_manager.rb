bec49850b56a982559581a454d39644f095a1a92
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-08-26 00:53:50 +0100

    iteration 2, extract zipcode function

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,15 +1,33 @@
 require 'csv'
 
-puts 'EventManager initialized.'
+# def clean_zipcode(zipcode)
+#     if zipcode.nil?
+#       '00000'
+#     elsif zipcode.length < 5
+#       zipcode.rjust(5, '0')
+#     elsif zipcode.length > 5
+#       zipcode[0..4]
+#     else
+#       zipcode
+#     end
+#   end
 
-contents = CSV.open(
+def clean_zipcode(zipcode)
+    zipcode.to_s.rjust(5, '0')[0..4]
+  end
+  
+  puts 'EventManager initialized.'
+  
+  contents = CSV.open(
     'event_attendees.csv',
     headers: true,
     header_converters: :symbol
   )
   
-contents.each do |row|
+  contents.each do |row|
     name = row[:first_name]
-    zipcode = row[:zipcode]
+  
+    zipcode = clean_zipcode(row[:zipcode])
+  
     puts "#{name} #{zipcode}"
-end
+  end
